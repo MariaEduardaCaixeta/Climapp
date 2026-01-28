00146db4050e393f6c3623b18683ff4efdc51512
Maria Eduarda Caixeta <mariaeduarda.caixeta@performait.com>
2026-01-28 11:36:01 +0000

    creating home screen

--- climapp/app/index.tsx
+++ climapp/app/index.tsx
@@ -1,15 +1,65 @@
-import { Text, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { LinearGradient } from "expo-linear-gradient";
+import { Image, Pressable, StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+    <LinearGradient
+      colors={['#00457D', '#05051F']}
+      style={styles.container}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+      <SafeAreaView style={styles.wrapper}>
+        <Image
+          source={require('../assets/images/logo.png')}
+        />
+        <Image
+          source={require('../assets/images/weather.png')}
+        />
+        <Text style={styles.welcome}>Boas-vindas!</Text>
+        <Pressable style={styles.buttonContainer}>
+          {({ pressed }) => (
+            <View style={[styles.button, { opacity: pressed ? 0.5 : 1 }]}>
+              <Text style={styles.buttonText}>Entrar</Text>
+              <Ionicons name="arrow-forward" size={24} color="#01080E" />
+            </View>
+          )}
+        </Pressable>
+      </SafeAreaView>
+    </LinearGradient>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  wrapper: {
+    flex: 1,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    width: '80%',
+    alignSelf: 'center',
+  },
+  welcome: {
+    fontSize: 25,
+    color: '#FFFFFF'
+  },
+  buttonContainer: {
+    width: '100%'
+  },
+  button: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#7693FF',
+    gap: 8,
+    padding: 8,
+    borderRadius: 32,
+  },
+  buttonText: {
+    color: '#01080E',
+    fontSize: 20,
+    fontWeight: 500
+  }
+});
